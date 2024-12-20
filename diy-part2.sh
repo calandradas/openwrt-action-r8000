541475fa85375bfd67f88ee5e0f225dc297ce316
Calandra Das <154507925+calandradas@users.noreply.github.com>
2024-12-20 10:16:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,25 +16,5 @@
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-rm -rf feeds/kenzo/luci-theme-argon feeds/kenzo/luci-app-argon-config
-git clone https://github.com/jerrykuku/luci-theme-argon.git feeds/kenzo/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git feeds/kenzo/luci-app-argon-config
-
-# goland 2.1 to golang 2.2
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-
-# miniupnpd 2.3.7 to 2.1.20200510
-#rm -fr feeds/packages/net/miniupnpd
-#git clone --filter=blob:none --no-checkout https://github.com/openwrt/packages miniupnpd-tmp
-#cd miniupnpd-tmp
-#git sparse-checkout init --cone
-#git sparse-checkout set net/miniupnpd
-#git checkout openwrt-18.06
-#cp -r net/miniupnpd ../feeds/packages/net/
-#cd ..
-#rm -fr miniupnpd-tmp
-
-
 # Modify hostname
 sed -i 's/OpenWrt/R8000Wrt/g' package/base-files/files/bin/config_generate
